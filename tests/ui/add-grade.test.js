b20293ae905426664df545e5e5c97de7fc0c0b71
Milen Ivanov <milensl92@gmail.com>
2024-04-07 15:55:47 +0300

    Fix the selector in add-grade.test

--- tests/ui/add-grade.test.js
+++ tests/ui/add-grade.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check add grades page', async ({ page }) => {
     await page.goto('http://localhost:8090/Add-Grade');
-    const form = await page.$('input');
+    const form = await page.$('form');
     expect(form).toBeTruthy();
   });
   
